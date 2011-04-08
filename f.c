dee35619a27656cf46ea781bb7dfb7c8375f9395
Renbo <renbo4113@126.com>
2011-04-08 18:56:24 +0800

    ---

--- f.c
+++ f.c
@@ -1,18 +1,18 @@
  #include <stdio.h>
- int add(int a,int b);
+ int sub(int a,int b);
 
  int main(int argc, const char *argv[])
  {
      int a = 5;
-     int b = 6;
+     int b = 3;
      int c = 0;
 
-     c = add(a,b);
+     c = sub(a,b);
      printf("c = %d\n",c);
      return 0;
  }
  
-int add(int a ,int b)     
+int sub(int a ,int b)     
 {
-    return (a+b);
+    return (a-b);
 }
