421cc32fee474990a04a1d642f12761d67389095
Renbo <renbo4113@126.com>
2011-04-08 18:34:47 +0800

    2

--- f.c
+++ f.c
@@ -1,1 +1,18 @@
+ #include <stdio.h>
+ int add(int a,int b);
+
+ int main(int argc, const char *argv[])
+ {
+     int a = 5;
+     int b = 6;
+     int c = 0;
+
+     c = add(a,b);
+     printf("c = %d\n",c);
+     return 0;
+ }
  
+int add(int a ,int b)     
+{
+    return (a+b);
+}
