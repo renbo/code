6d2bcf64d61cd0460db224ef0f05dc66c5aed24c
Renbo <renbo4113@126.com>
2011-04-08 20:18:33 +0800

    while

--- f.c
+++ f.c
@@ -3,31 +3,30 @@
 int main(int argc, const char *argv[])
 {
     int array[100];
-
     init_array(array,100);
     printf_array(array,100);
+
     return 0;
 }
-int init_array(int p[],int n) 
+int init_array(int p[],int n)
 {
     int i =0;
-    for (i = 0; i < n; i++) 
+    i = 0;
+    while(i<n)
     {
         p[i] = i+1;
+        i++;
     }
     return 0;
-
 }
 int printf_array(int p[],int n)
 {
     int i = 0;
-    for (i = 0; i < n; i++)
-
-    {
+    i = 0;
+    while(i<n)
+    { 
         printf("%5d",p[i]);
-
+        i++;
     }
     printf("\n");
 }
-
-
