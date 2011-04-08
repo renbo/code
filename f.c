327b377712d55d8222048589e65354b5ff256a2e
Renbo <renbo4113@126.com>
2011-04-08 19:07:34 +0800

    /

--- f.c
+++ f.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-int mul(int a,int b);
+int div(int a,int b);
 int main(int argc, const char *argv[])
 {
-    int a = 8;
-    int b = 2;
+    int a = 6;
+    int b = 3;
     int c = 0;
 
-    c = mul(a,b);
-    printf("c=%d\n",c);
-
+    c = div(a,b);
+    printf("c =%d\n",c);
+    
     return 0;
-
 }
-int mul(int a,int b)
+int div(int a,int b)
 {
-    return(a*b);
+    return (a/b);
 }
